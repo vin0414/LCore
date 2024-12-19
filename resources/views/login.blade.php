e80f6bc0beb1f14fe2aa036eabaa88919d1cd2ab
vin0414 <vinmogate@gmail.com>
2024-12-19 16:47:38 +0800

    add meta

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="{{isset($about['companyDetails']) ? $about['companyDetails'] : 'No Available Details' }}">
+    <meta name="keywords" content="{{isset($about['companyName']) ? $about['companyName'] : 'No Available keywords' }}">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
